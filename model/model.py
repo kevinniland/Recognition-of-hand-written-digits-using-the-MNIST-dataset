37882f295b6dae51a5df68bb26bbefd6fcca6182
Kevin Niland <G00342279@gmit.ie>
2019-10-24 16:52:01 +0100

    Updated model file and added comments

--- model/model.py
+++ model/model.py
@@ -1,14 +1,41 @@
-# Imports
-import matplotlib.pyplot as plt
+# Matplotlib is a Python 2D plotting library which produces publication quality figures in a variety of 
+# hardcopy formats and interactive environments across platforms
+import matplotlib.pyplot as plt 
+
+# NumPy is the fundamental package for scientific computing with Python
 import numpy as np
 
 # Keras is a high-level neural networks API, written in Python and capable of running on top of TensorFlow, CNTK, or Theano
 import keras
-from keras.datasets import mnist # Import the MNIST dataset directly from the Keras API
-from keras.models import Sequential # The Sequential model is a linear stack of layers
+
+# Import the MNIST dataset directly from the Keras API
+from keras.datasets import mnist 
+
+# The Sequential model is a linear stack of layers
+from keras.models import Sequential 
+
+''' Please refer to the wiki of the repository from which this project is located for a more in-depth analysis and explanation
+    of various imports, packages, the model, how it is trained, etc. '''
+# Dense - implements the operation: output = activation(dot(input, kernel) + bias) where activation is the element-wise 
+# activation function passed as the activation argument, kernel is a weights matrix created by the layer, and bias is a bias 
+# vector created by the layer (only applicable if use_bias is True)
+
+# Dropout - applies Dropout to the input. This consists of randomly setting a fraction rate of input units to 0 
+# at each update during training time, which helps prevent overfitting
+
+# Flatten - 'flattens' the input. Does not affect the batch size. Flattening a tensor means to remove all of the dimensions except for one
+# Conv2D - 2D convolution layer (e.g. spatial convolution over images). Creates a convolution kernel that is convolved with the layer input 
+# to produce a tensor of outputs
+
+# MaxPooling2D - Max pooling operation for spatial data
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+
+# Allows for the saving and loading of the model
 from keras.models import load_model
 
+# Downloads the MNIST data set directly from the TensorFlow and Keras API. The MNIST dataset contains 60,000 training images
+# and 10,000 testing images (with accompanying labels). We separate these two groups into train_imgs and train_labels for the training
+# images and training labels, respectively, and test_imgs and test_labels for the test images and test labels respectively
 (train_imgs, train_labels), (test_imgs, test_labels) = mnist.load_data()
 
 # Defines the size of the image as 28 * 28 pixels
@@ -17,13 +44,7 @@ img_rows, img_cols = 28, 28
 # Reshaping
 #
 # To be able to use the MNIST dataset with the Keras API, we need to change our array (which is 3-dimensional)
-# to 4-dimensional numpy arrays. We also must 'normalize' our data, as is always required in neural networks.
-# This can be done by dividing the RGB codes of the images to 255
-# if k.image_data_format() == 'channels_first':
-#     train_imgs = train_imgs.reshape(train_imgs.shape[0], 1, img_rows, img_cols)
-#     test_imgs = test_imgs.reshape(test_imgs.shape[0], 1, img_rows, img_cols)
-#     input_shape = (1, img_rows, img_cols)
-# else:
+# to 4-dimensional numpy arrays 
 train_imgs = train_imgs.reshape(train_imgs.shape[0], img_rows, img_cols, 1)
 test_imgs = test_imgs.reshape(test_imgs.shape[0], img_rows, img_cols, 1)
 input_shape = (img_rows, img_cols, 1)
@@ -32,15 +53,17 @@ input_shape = (img_rows, img_cols, 1)
 train_imgs = train_imgs.astype('float32')
 test_imgs = test_imgs.astype('float32')
 
+# We also must 'normalize' our data, as is always required in neural networks.
+# This can be done by dividing the RGB codes of the images to 255
 train_imgs /= 255
 test_imgs /= 255
-
-print(train_imgs.shape[0], "training samples")
-print(test_imgs.shape[0], "testing samples")
 
 # In the model, we can experiment with any number for the first Dense layer. However, the final Dense layer must have
 # 10 neurons since there are 10 number classes (0, 1, 2, 3, ..., 9)
 num_classes = 10
+
+# 
+rate = 0.5
 
 train_labels = keras.utils.to_categorical(train_labels, num_classes)
 test_labels = keras.utils.to_categorical(test_labels, num_classes)
@@ -56,7 +79,7 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 model.add(Flatten())  # Flattens the 2D arrays for fully connected layers
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(rate))
 model.add(Dense(num_classes, activation='softmax'))
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
@@ -66,14 +89,16 @@ batch_size = 128
 
 # In the context of a neural network, one epoch is the equivalent of one forward pass and one backward pass of
 # all the training examples
-num_epoch = 100
+
+# With this model, the model seems to peak (in terms of accuracy) at 20 epochs. Any more epochs after this is more or less overkill
+num_epoch = 20
 
 # To avoid having to train the model each time the program is ran, the trained model can be loaded from a file
 # If no file is created, then the model is trained and then saved to a file
 try:
     print("Attempting to load model....")
     print("Model loaded successfully")
-    model = load_model("model_digit.h5")
+    model = load_model("model.h5")
 except:
     print("Failed to load model. Creating new model...")
     model_log = model.fit(train_imgs, train_labels,
@@ -82,9 +107,9 @@ except:
                           verbose=1,
                           validation_data=(test_imgs, test_labels))
 
-    model.save_weights("model_digit.h5")
+    model.save_weights("model.h5")
 
-    model.save("model_digit.h5")
+    model.save("model.h5")
     print("Saved model. Model will now be loaded on next run through")
 
 plt.imshow(test_imgs[999].reshape(28, 28), cmap="gray")
